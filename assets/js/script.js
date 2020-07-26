8832dc830197f00d4265486f69eff3306cd2a658
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-07-26 15:19:47 -0500

    added projects: Doggypedia, Weather Dashboard, Work Day Scheduler

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$("#main").on("click", function(){
+    console.log("main - doggy was clicked")
+    window.open("https://kray306090.github.io/yorkie-terrier/")
+
+})
+       
+$("#proj1").on("click", function(){
+    console.log("proj1 - run buddy was clicked")
+    window.open("https://mlportu.github.io/run-buddy")
+
+})
+       
+$("#proj2").on("click", function(){
+    console.log("proj2 - work daywas clicked")
+    window.open("https://mlportu.github.io/workday-scheduler/")
+
+})
+       
+$("#proj3").on("click", function(){
+    console.log("proj3 - weather was clicked")
+    window.open("https://mlportu.github.io/weather-dashboard/")
+
+})
+
+
+       
