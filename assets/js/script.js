05782e590aa906bf33179e071a7112db779445bd
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-09-20 19:35:47 -0500

    project 2 added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,28 @@
 $("#main").on("click", function(){
-    console.log("main - doggy was clicked")
-    window.open("https://kray306090.github.io/yorkie-terrier/")
-
-})
+    // console.log("main - healthlog was clicked")
+    window.open("https://fathomless-thicket-41464.herokuapp.com")
+});
        
 $("#proj1").on("click", function(){
-    console.log("proj1 - run buddy was clicked")
-    window.open("https://mlportu.github.io/run-buddy")
+    // console.log("main - doggy was clicked")
+    window.open("https://kray306090.github.io/yorkie-terrier/")
 
-})
+});
        
 $("#proj2").on("click", function(){
-    console.log("proj2 - work daywas clicked")
+    // console.log("proj2 - work day was clicked")
     window.open("https://mlportu.github.io/workday-scheduler/")
 
-})
+});
        
 $("#proj3").on("click", function(){
-    console.log("proj3 - weather was clicked")
+    // console.log("proj3 - weather was clicked")
     window.open("https://mlportu.github.io/weather-dashboard/")
 
-})
+});
 
+$("#proj4").on("click", function(){
+    // console.log("proj4 - run buddy was clicked")
+    window.open("https://mlportu.github.io/run-buddy")
 
-       
+});      
